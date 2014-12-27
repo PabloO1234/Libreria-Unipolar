3048ac69fdd5218bb3777bcbb0b3a1ea366c7bd5
Pablo Otón Zamora <otonpablo@gmail.com>
2014-12-27 19:36:40 +0100

    Update Ejemplo_usando_las_3_funciones.ino

--- Libreria_Unipolar/examples/Ejemplo_usando_las_3_funciones/Ejemplo_usando_las_3_funciones.ino
+++ Libreria_Unipolar/examples/Ejemplo_usando_las_3_funciones/Ejemplo_usando_las_3_funciones.ino
@@ -5,10 +5,10 @@ Unipolar motor (10,11,12,13); // Creamos el objeto 'motor' asociado a 'Unipolar'
                             // 4 pines digitales que queramos, siguiendo el siguiente orden;
                             // (naranja, amarillo, rosa, azul) 
                             // En este caso: 
-                            //// cable naranja->pin 8
-                            //// cable amarillo->pin 9
-                            //// cable rosa->pin 10
-                            //// cable azul->pin 11
+                            //// cable naranja->pin 10
+                            //// cable amarillo->pin 11
+                            //// cable rosa->pin 12
+                            //// cable azul->pin 13
 void setup(){}
 void loop(){
   motor.derecha(); //Giramos a la derecha
